03801d198697e00b278a2028e884abcd1afbb4cd
AlberchtCa <andiconstandache@gmail.com>
2019-12-13 21:38:00 +0200

    First Commit

---
+++ DAWOO/Startup.cs
@@ -0,0 +1,59 @@
+﻿using DAWOO.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DAWOO.Startup))]
+namespace DAWOO
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+            createAdminUserAndApplicationRoles(); 
+        }
+
+        private void createAdminUserAndApplicationRoles()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+        
+            if(!roleManager.RoleExists("Administrator"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Administrator";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "admin@admin.com";
+                user.Email = "admin@admin.com";
+
+                var adminCreated = UserManager.Create(user, "Administrator1!");
+                if (adminCreated.Succeeded)
+                {
+                    UserManager.AddToRole(user.Id, "Administrator");
+                }
+            }
+
+            if (!roleManager.RoleExists("Guest"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Guest";
+                roleManager.Create(role);
+            }
+
+            if(!roleManager.RoleExists("User"))
+            {
+                var role = new IdentityRole();
+                role.Name = "User";
+                roleManager.Create(role);
+            }
+        }
+    }
+
+}
