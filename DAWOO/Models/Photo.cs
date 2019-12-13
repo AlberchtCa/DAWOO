b3ec62aaffcd20e0999bf86c38ce276c2c1893f2
AlberchtCa <33205298+AlberchtCa@users.noreply.github.com>
2019-12-13 21:30:27 +0000

    Added the Photo model

---
+++ DAWOO/Models/Photo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DAWOO.Models
+{
+    public class Photo
+    {
+        public int ID { get; set; }
+        public DateTime PostDate { get; set; }
+        public string Description { get; set; }
+        public byte[] Image { get;set }
+
+        public int CategoryId { get; set; }
+
+        public string UserId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        public class PhotoDBContext : DbContext
+        {
+            public PhotoDBContext() : base("DBConnectionString") { }
+            public DbSet<Photo> Photos { get; set; }
+            public DbSet<Category> Categories { get; set; }
+        }
+    }
+}
